d3be24411108c3f31b5c28e7e08d7d6b2982d384
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2024-10-10 22:42:10 +0000

    Add project files.

---
+++ ountryDataDotnetDemo.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ountryDataDotnetDemo.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
